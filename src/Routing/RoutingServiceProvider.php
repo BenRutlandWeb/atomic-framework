f0357dc11e4f619e2680ec2e34012a1d069e43ae
Ben Rutland <benrutland@hotmail.com>
2021-01-24 13:57:26 +0000

    removed commented code

--- src/Routing/RoutingServiceProvider.php
+++ src/Routing/RoutingServiceProvider.php
@@ -29,8 +29,6 @@ class RoutingServiceProvider extends ServiceProvider
             // The URL generator needs the route collection that exists on the router.
             // Keep in mind this is an object, so we're passing by references here
             // and all the registered routes will be available to the generator.
-            #$app->instance('routes', $routes, $ajaxRoutes);
-
             return new UrlGenerator(
                 $routes,
                 $ajaxRoutes,
